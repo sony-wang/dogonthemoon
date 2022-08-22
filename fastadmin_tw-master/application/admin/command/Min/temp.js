0f24bb376d8c88194b9d261ba571d5fd57389443
sony <phenombox@gmail.com>
2022-08-22 19:12:10 +0800

    delete something

--- fastadmin_tw-master/application/admin/command/Min/temp.js
+++
@@ -1,6 +0,0 @@
-({
-  cssIn: "C:/AppServ/www/icare/public/assets/css/backend.css",
-  out: "C:/AppServ/www/icare/public/assets/css/backend.min.css",
-  optimizeCss: "default",
-  optimize: "none"
-})
