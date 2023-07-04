ba8e29018e0ca635083eb3f34f4ddc42511ad20b
Chao <inm20901@yahoo.com.tw>
2023-07-04 14:56:05 +0800

    上傳

--- public/assets/js/frontend/adopt.js
+++ public/assets/js/frontend/adopt.js
@@ -7,6 +7,14 @@ define(['jquery', 'bootstrap', 'frontend', 'form', 'template'], function ($, und
         }
     };
     var Controller = {
+        info: function () {
+            Form.api.bindevent($("#adopt-form"),function (mthis, data, ret) {
+                if(data.code === 1){
+                    
+                }
+                // location.href = Config.url.furl+'/index/donate/orderpage/number/'+data.data;
+            });
+        }
     };
     return Controller;
 });
